fec3925c0c0ad854e31167129a5bb26b4db5685a
paulc <paulc@>
2021-12-05 18:53:23 +0000

    day4/5

---
+++ day04/code.js
@@ -0,0 +1,104 @@
+
+import * as std from "std";
+import * as os from "os";
+
+const test = `
+7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7
+`
+
+function parseInput(data) {
+    data = data.trim().split(/\n+/)
+    const lines = data.slice()
+    const numbers = lines.shift().split(",").map((c) => parseInt(c))
+    const boards = []
+    const getRow = () => lines.shift().trim().split(/ +/).map((c) => parseInt(c))
+    while (lines.length) {
+        boards.push({
+            numbers: new Array(5).fill(undefined).flatMap(() => getRow()),
+            marked: new Array(),
+            markedRows: new Array(5).fill(0),
+            markedCols: new Array(5).fill(0),
+            win: false
+        })
+    }
+    return { numbers: numbers, boards: boards } 
+}
+
+function markBoard(board,draw) {
+    const pos = board.numbers.indexOf(draw)
+    if (pos !== -1) {
+        const col = pos % 5
+        const row = Math.trunc(pos/5)
+        board.marked.push(draw)
+        ++board.markedRows[Math.trunc(pos/5)]
+        ++board.markedCols[pos%5]
+        board.win = board.markedRows.includes(5) || board.markedCols.includes(5)
+    }
+}
+
+function sum(a) {
+    return a.reduce((prev,cur) => prev += cur)
+}
+
+function part1(data) {
+    const bingo = parseInput(data)
+    for (const draw of bingo.numbers) {
+        for (const board of bingo.boards) {
+            markBoard(board,draw)
+            if (board.win) {
+                return (sum(board.numbers) - sum(board.marked)) * draw
+            }
+        }
+    }
+}
+
+function part2(data) {
+    const bingo = parseInput(data)
+    let lastWin
+    for (const draw of bingo.numbers) {
+        for (const board of bingo.boards) {
+            if (!board.win) {
+                markBoard(board,draw)
+                if (board.win) {
+                    lastWin = { board:board, draw:draw }
+                }
+            }
+        }
+    }
+    return (sum(lastWin.board.numbers) - sum(lastWin.board.marked)) * lastWin.draw
+}
+
+const part1_expected = 4512
+const part2_expected = 1924
+
+if (part1(test) !== part1_expected) {
+    throw("Part1 Test Failed")
+}
+
+if (part2(test) !== part2_expected) {
+    throw("Part2 Test Failed")
+}
+
+const data = std.loadFile("input.txt")
+
+print("Part1:",part1(data))
+print("Part2:",part2(data))
+
---
+++ day05/code.js
@@ -0,0 +1,80 @@
+
+import * as std from "std";
+import * as os from "os";
+
+const test = `
+0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+`
+
+function parseInput(data) {
+    const parseXY = (s) => s.split(",").map((n) => parseInt(n))
+    const parseLine = (s) => s.split(" -> ").map((s) => parseXY(s))
+    return data.trim().split("\n").map((s) => parseLine(s))
+}
+
+function setupBoard(data) {
+    const lines = parseInput(data)
+    const maxX = lines.flat().reduce((p,[x,y]) => x > p ? x : p, 0) + 1
+    const maxY = lines.flat().reduce((p,[x,y]) => y > p ? y : p, 0) + 1
+    const board = new Array(maxX*maxY).fill(0)
+    const convXY = (x,y) => x + (y * maxX)
+    return [lines,board,convXY]
+}
+
+function drawLine(line,board,convXY) {
+    const [[x1,y1],[x2,y2]] = line
+    if (x1 === x2) {
+        for (let y = Math.min(y1,y2); y <= Math.max(y1,y2); ++y) {
+            ++board[convXY(x1,y)]
+        }
+    } else if (y1 === y2) {
+        for (let x = Math.min(x1,x2); x <= Math.max(x1,x2); ++x) {
+            ++board[convXY(x,y1)]
+        }
+    } else {
+        const [startX,endX,startY,endY] = (x1 > x2) ? [x2,x1,y2,y1] : [x1,x2,y1,y2]
+        const dy = (endY > startY) ? 1 : -1
+        for (let x = startX, y = startY; x <= endX; ++x, y += dy) {
+            ++board[convXY(x,y)]
+        }
+    }
+}
+
+function part1(data) {
+    const [lines,board,convXY] = setupBoard(data)
+    lines.filter(([[x1,y1],[x2,y2]]) => (x1 === x2) || (y1 === y2)).map((line) => drawLine(line,board,convXY))
+    return board.filter((c) => c > 1).length
+}
+
+function part2(data) {
+    const [lines,board,convXY] = setupBoard(data)
+    lines.map((line) => drawLine(line,board,convXY))
+    return board.filter((c) => c > 1).length
+}
+
+const data = std.loadFile("input.txt")
+
+const part1_expected = 5
+const part2_expected = 12
+
+if (part1(test) !== part1_expected) {
+    throw("Part1 Test Failed")
+}
+
+print("Part1:",part1(data))
+
+if (part2(test) !== part2_expected) {
+    throw("Part2 Test Failed")
+}
+
+print("Part2:",part2(data))
+
