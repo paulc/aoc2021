595f2038b3b47ce158df1d018b0559d671a4576f
paulc <paulc@>
2021-12-10 11:47:21 +0000

    day10

--- day09/code.js
+++ day09/code.js
@@ -10,26 +10,50 @@ const test = `
 9899965678
 `
 
-function *range(start,end,step=1) {
-    for (let i = ((end === undefined) ? 0 : start);
-         (step < 0) ? i > ((end === undefined) ? start : end):
-                      i < ((end === undefined) ? start : end);
-         i += step) {
-        yield i
+function *genXY(maxX,maxY) {
+    for (let x = 0; x < maxX; ++x) {
+        for (let y = 0; y < maxY; ++y) {
+            yield [x,y]
+        }
     }
 }
 
 function parseInput(data) {
+    return data.trim().split("\n").map((l) => l.split("").map((v) => parseInt(v)))
+}
+
+function getAdjacent(map,x,y) {
+    return [[x-1,y],[x+1,y],[x,y-1],[x,y+1]].filter(([x,y]) => x >= 0 && x < map[0].length && y >= 0 && y < map.length)
+}
+
+function isLower(map,x,y) {
+    return getAdjacent(map,x,y).map(([x1,y1]) => map[y1][x1]).map((v) => map[y][x] < v).reduce((cur,prev) => cur && prev)
+}
+
+function findBasin(map,x,y) {
+    let basin = new Set([[x,y].toString()])
+    for (const [x1,y1] of getAdjacent(map,x,y)) {
+        if (map[y1][x1] > map[y][x] && map[y1][x1] !== 9) {
+            findBasin(map,x1,y1).forEach((v) => basin.add(v))
+        }
+    }
+    return basin
 }
 
 function part1(data) {
+    const map = parseInput(data)
+    return Array.from(genXY(map[0].length,map.length),([x,y]) => isLower(map,x,y) ? map[y][x]+1 : 0).reduce((cur,prev) => cur + prev)
 }
 
 function part2(data) {
+    const map = parseInput(data)
+    const lowPoints = Array.from(genXY(map[0].length,map.length)).filter(([x,y]) => isLower(map,x,y))
+    const basins = lowPoints.map(([x,y]) => findBasin(map,x,y).size).sort((a,b) => a - b)
+    return basins.slice(-3).reduce((cur,prev) => cur * prev)
 }
 
-const part1_expected = 37
-const part2_expected = 168
+const part1_expected = 15
+const part2_expected = 1134
 
 if (part1(test) !== part1_expected) {
     throw("Part1 Test Failed")
---
+++ day10/code.js
@@ -0,0 +1,85 @@
+
+import * as std from "std";
+import * as os from "os";
+
+const test = `
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]
+`
+
+function parseInput(data) {
+    return data.trim().split("\n")
+}
+
+function checkLine(line) {
+    const stack = new Array()
+    const open = ["(","[","{","<"]
+    const close = [")","]","}",">"]
+    const score = [3,57,1197,25137]
+    for (const c of line) {
+        if (open.includes(c)) {
+            stack.push(c) 
+        } else if (close.includes(c)) {
+            if (stack.pop() !== open[close.indexOf(c)]) {
+                return score[close.indexOf(c)]
+            }
+        } else {
+            throw "Invalid character: " + c
+        }
+    }
+    return 0
+}
+
+function fixLine(line) {
+    const stack = new Array()
+    const open = ["(","[","{","<"]
+    const close = [")","]","}",">"]
+    const score = [1,2,3,4]
+    for (const c of line) {
+        if (open.includes(c)) {
+            stack.push(c) 
+        } else if (close.includes(c)) {
+            if (stack.pop() !== open[close.indexOf(c)]) {
+                return 0
+            }
+        } else {
+            throw "Invalid character: " + c
+        }
+    }
+    return stack.reverse().map((c) => score[open.indexOf(c)]).reduce((prev,cur) => prev*5 + cur)
+}
+
+function part1(data) {
+    return parseInput(data).map((l) => checkLine(l)).reduce((cur,prev) => cur + prev)
+}
+
+function part2(data) {
+    const scores = parseInput(data).map((l) => fixLine(l)).filter((s) => s > 0).sort((a,b) => a-b)
+    return scores[Math.floor(scores.length/2)]
+}
+
+const part1_expected = 26397
+const part2_expected = 288957
+
+if (part1(test) !== part1_expected) {
+    throw("Part1 Test Failed")
+}
+
+const data = std.loadFile("input.txt")
+
+print("Part1:",part1(data))
+
+if (part2(test) !== part2_expected) {
+    throw("Part2 Test Failed")
+}
+
+print("Part2:",part2(data))
+
