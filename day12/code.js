fce0cab6f156d29e5d82193926d59b0a8305c32e
paulc <paulc@>
2021-12-13 17:52:56 +0000

    day12-slow

--- day12/code.js
+++ day12/code.js
@@ -47,33 +47,65 @@ start-RW
 `
 
 function parseInput(data) {
-    // Add reverse paths
-    return data.trim().split("\n").flatMap((line) => [line.split("-"),line.split("-").reverse()])
+    // Add reverse paths and remove paths returning to start
+    return data.trim().split("\n").flatMap((line) => [line.split("-"),line.split("-").reverse()]).filter(([start,end]) => end !== "start")
 }
 
-function findPath(visited,paths) {
+Map.prototype.incr = function(k) { this.has(k) ? this.set(k,this.get(k)+1) : this.set(k,1); return this.get(k) }
+Map.prototype.getDefault = function(k,d) { return this.has(k) ? this.get(k) : d }
+
+function findPath(visited,paths,small,done) {
     const last = visited[visited.length-1]
     if (last === "end") {
-        return visited
+        done.add(visited.join(","))
     } else {
-        const valid = paths.filter(([start,end]) => (start === last && !visited.filter((n) => /^[a-z]+$/.test(n)).includes(end)))
-        return(valid.map(([_,next]) => findPath(Array.from([...visited,next]),paths)))
+        if (/^[a-z]+$/.test(last)) {
+            small = new Set([...small,last])
+        }
+        const valid = paths.filter(([start,end]) => (start === last && !small.has(end)))
+        valid.map(([_,next]) => findPath(Array.from([...visited,next]),paths,small,done))
+    }
+}
+
+function findPath2(visited,paths,small,done) {
+    const last = visited[visited.length-1]
+    if (last === "end") {
+        done.add(visited.join(","))
+    } else {
+        if (/^[a-z]+$/.test(last)) {
+            small = new Map([...small])
+            small.incr(last)
+        }
+        if (Array.from(small.values()).reduce((prev,cur) => cur > 1 ? prev + 1 : prev,0) > 1) {
+            // skip
+        } else {
+            const valid = paths.filter(([start,end]) => (start === last && !(small.getDefault(end,0) > 1)))
+            valid.map(([_,next]) => findPath2(Array.from([...visited,next]),paths,small,done))
+        }
     }
 }
 
 function part1(data) {
     const paths = parseInput(data)
-    return findPath(["start"],paths).flat(Infinity).filter((n) => n === "end").length
+    const small = new Set()
+    const done = new Set()
+    findPath(["start"],paths,small,done)
+    return done.size
 }
 
 function part2(data) {
+    const paths = parseInput(data)
+    const small = new Map()
+    const done = new Set()
+    findPath2(["start"],paths,small,done)
+    return done.size
 }
 
 const part1_expected_a = 10
 const part1_expected_b = 19
 const part1_expected_c = 226
 
-const part2_expected_a = 10
+const part2_expected_a = 36
 const part2_expected_b = 103
 const part2_expected_c = 3509
 
