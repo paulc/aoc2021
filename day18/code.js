ab26e05559c69d1569d47a6691fafa441f374489
paulc <paulc@>
2021-12-19 13:21:13 +0000

    day18p1

--- day18/code.js
+++ day18/code.js
@@ -3,10 +3,24 @@ import * as std from "std";
 import * as os from "os";
 
 const test = `
+[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
 `
 
 const isPair = (v) => Pair.prototype.isPrototypeOf(v)
 const isArray = (v) => Array.prototype.isPrototypeOf(v)
+
+function parseInput(data) {
+    return data.trim().split("\n").map((l) => eval(l)).map((n) => new Pair(n))
+}
 
 class Pair {
     constructor(LR,parent=undefined) {
@@ -34,77 +48,57 @@ class Pair {
         }
         return count
     }
-    /*
-    getL() {
-        let p = this
-        while (isPair(p.L)) {
-            p = p.L
+    _explode() {
+        const [adjL,adjR] = this.adjacent()
+        if (adjL) {
+            adjL.p[adjL.s] += this.L
         }
-        return p.L
-    }
-    getR() {
-        let p = this
-        while (isPair(p.R)) {
-            p = p.R
+        if (adjR) {
+            adjR.p[adjR.s] += this.R
         }
-        return p.R
-    }
-    getAdjL() {
-        if (this.parent === undefined || this.parent.L === this) {
-            return undefined
-        }
-        return this.parent.getL()
-    }
-    getAdjR() {
-        if (this.parent === undefined || this.parent.R === this) {
-            return undefined
-        }
-        return this.parent.getL()
-    }
-    */
-    addL(val) {
-        if (this.parent === undefined || this.parent.L === this) {
-            return undefined
-        }
-        let p = this.parent
-        while (isPair(p.L)) {
-            p = p.L
-        }
-        return (p.L += val)
-    }
-    addR(val) {
-        if (this.parent === undefined || this.parent.R === this) {
-            return undefined
-        }
-        let p = this.parent
-        while (isPair(p.R)) {
-            p = p.R
-        }
-        return (p.R += val)
-    }
-    explode() {
-        this.addL(this.L)
-        this.addR(this.R)
         this.parent.L === this ? this.parent.L = 0 : this.parent.R = 0
         return true
     }
-    split() {
-        if (!this.isPair(this.L) && this.L > 10) {
+    _split(side) {
+        if (side === "L") {
             this.setChild("L",new Pair([Math.floor(this.L/2),Math.ceil(this.L/2)]))
         } else {
             this.setChild("R",new Pair([Math.floor(this.R/2),Math.ceil(this.R/2)]))
         }
+        return true
     }
-    explodePairs() {
+    explode() {
         const p = Array.from(this.walk()).filter((p) => p.depth() === 4).shift()
-        return p ? p.explode() : false
+        return p ? p._explode() : false
     }
-    splitPairs() {
-        const p = Array.from(p.walk()).filter((p) => (isPair(p.L) ? false : p.L > 10) || (isPair(p.R) ? false : p.R > 10)).shift()
-        return p ? p.split() : false
+    split() {
+        const l = this.leaves().filter((p) => p.v >= 10).shift()
+        return l ? l.p._split(l.s) : false
+    }
+    reduce() {
+        while (this.explode() || this.split()) {}
+        return this
     }
     flat() {
         return [ isPair(this.L) ? this.L.flat() : this.L, isPair(this.R) ? this.R.flat() : this.R ] 
+    }
+    leaves() {
+        return [ isPair(this.L) ? this.L.leaves() : {p:this,s:"L",v:this.L}, isPair(this.R) ? this.R.leaves() : {p:this,s:"R",v:this.R} ].flat()
+    }
+    root() {
+        let root = this
+        while (root.parent) {
+            root = root.parent
+        }
+        return root
+    }
+    adjacent() {
+        const leaves = this.root().leaves()
+        const map = leaves.map((l) => l.p === this)
+        return [ leaves[map.indexOf(true)-1], leaves[map.lastIndexOf(true)+1] ]
+    }
+    magnitude() {
+        return 3 * (isPair(this.L) ? this.L.magnitude() : this.L) + 2 * (isPair(this.R) ? this.R.magnitude() : this.R)
     }
     *walk(f) {
         yield f ? f(this) : this
@@ -117,24 +111,21 @@ class Pair {
         }
     }
     toString() {
-        return JSON.stringify(this.flatten())
+        return JSON.stringify(this.flat())
     }
 }
 
-globalThis.Pair = Pair
-
-/*
-function parseInput(data) {
-}
-
 function part1(data) {
+    const numbers = parseInput(data)
+    const result = numbers.reduce((prev,cur) => new Pair([prev,cur]).reduce())
+    return result.magnitude()
 }
 
 function part2(data) {
 }
 
-const part1_expected = 45
-const part2_expected = 112
+const part1_expected = 4140
+const part2_expected = 3993
 
 if (part1(test) !== part1_expected) {
     throw("Part1 Test Failed")
@@ -149,4 +140,3 @@ if (part2(test) !== part2_expected) {
 }
 
 print("Part2:",part2(data))
-*/
