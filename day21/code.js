4a3f18e1c6b85683c5a7475c789b380b6cef9858
paulc <paulc@>
2021-12-22 18:14:47 +0000

    day21

--- day21/code.js
+++ day21/code.js
@@ -44,51 +44,42 @@ const decodeState = (n) => [(n>>21)&15,(n>>17)&15,(n>>9)&255,(n>>1)&255,n&1]
 const rolls = [1,2,3].flatMap((a) => [1,2,3].flatMap((b) => [1,2,3].flatMap((c) => a+b+c)))
 const score = (pos) => (pos === 0) ? 10 : pos
 
-let count = 0
-
 function quantum(states) {
-    let p1win = 0, p2win = 0
+    let win1 = 0, win2 = 0
     const next = new Map()
     for (const [state,count] of states) {
         const [p1,p2,s1,s2,turn] = decodeState(state)
-        if (turn === 0) {
-            for (const r of rolls) {
-                const new_pos = (p1 + r) % 10
-                const new_score = s1 + score(new_pos)
-                if (new_score >= 21) {
-                    p1win += count
-                } else {
-                    const next_state = encodeState(new_pos,p2,new_score,s2,1)
-                    next.incr(next_state,count)
-                }
-            }
-        } else {
-            for (const r of rolls) {
-                const new_pos = (p2 + r) % 10
-                const new_score = s2 + score(new_pos)
-                if (new_score >= 21) {
-                    p2win += count
-                } else {
-                    const next_state = encodeState(p1,new_pos,s1,new_score,0)
-                    next.incr(next_state,count)
-                }
+        for (const r of rolls) {
+            const new_pos = (turn === 0) ? (p1 + r) % 10 : (p2 + r) % 10
+            const new_score = (turn === 0) ? s1 + score(new_pos) : s2 + score(new_pos)
+            if (new_score >= 21 && turn === 0) {
+                win1 += count
+            } else if (new_score >= 21 && turn === 1) {
+                win2 += count
+            } else {
+                const next_state = encodeState((turn === 0) ? new_pos : p1,
+                                               (turn === 1) ? new_pos : p2, 
+                                               (turn === 0) ? new_score : s1,
+                                               (turn === 1) ? new_score : s2,
+                                               (turn ^ 1))
+                next.incr(next_state,count)
             }
         }
     }
-    return [next,p1win,p2win]
+    return [next,win1,win2]
 }
             
 function part2(data) {
     const [p1,p2] = parseInput(data)
     const start = encodeState(p1,p2,0,0,0)
     let states = new Map([[start,1]])
-    let p1tot = 0, p2tot = 0, p1win = 0, p2win = 0
+    let win1 = 0, win2 = 0, tot1 = 0, tot2 = 0
     while (states.size > 0) {
-        [states,p1win,p2win] = quantum(states)
-        p1tot += p1win
-        p2tot += p2win
+        [states,win1,win2] = quantum(states)
+        tot1 += win1
+        tot2 += win2
     }
-    return Math.max(p1tot,p2tot)
+    return Math.max(tot1,tot2)
 }
 
 const part1_expected = 739785
